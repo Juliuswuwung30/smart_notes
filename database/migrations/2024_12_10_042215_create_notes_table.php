9ac92c29447b9f6cd8aef36ee61a6b3c653a8245
GokaiInfinity <patriciofilbert@gmail.com>
2024-12-10 16:45:34 +0700

    refactor: changed user_id from database to auth0.com

--- database/migrations/2024_12_10_042215_create_notes_table.php
+++ database/migrations/2024_12_10_042215_create_notes_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->string('title');
             $table->text('content');
-            $table->foreignId('user_id')->constrained();
+            $table->uuid('user_id');
             $table->timestamps();
         });
     }
