f0146361387f31545072f11108f11ae59a0650d3
JuliusWuwung <127324193+Juliuswuwung30@users.noreply.github.com>
2024-12-30 20:24:30 +0700

    add-create-user

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoteController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/notes', [NoteController::class, 'getUserNotes']);
+Route::post('/notes', [NoteController::class, 'createNote'])->withoutMiddleware(VerifyCsrfToken::class);
